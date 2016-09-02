6a490db3b8b00745df1229b43d24aa39bffdcfdc
Yomi Eluwande <iamlese@yahoo.com>
2016-09-02 02:16:09 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(function(){
+    $("#element").typed({
+        stringsElement: $('#typing'),
+        typeSpeed: 50,
+        callback: function() {
+            $('div.hidden').fadeIn(1000).removeClass('hidden');
+        }
+    });
 
+});
